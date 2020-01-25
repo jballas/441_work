63f656681c2763b9af71aa3e6bd73ad0ef30a66c
Julia Ballas <42751526+jballas@users.noreply.github.com>
2020-01-25 09:58:30 -0700

    added details
    Added favorites to javascript console.log
    
    added some html for praciting css

--- HW-2/script.js
+++ HW-2/script.js
@@ -1,5 +1,9 @@
 // Homework Week 2
 // Julia Ballas
-console.log("Favorite Websites: https://www.youtube.com");
-console.log("Favorite Websites: https://www.polygon.com");
-console.log("")
+console.log("Favorite Websites 1: https://www.youtube.com");
+console.log("Favorite Websites 2: https://www.polygon.com");
+console.log("Favorite Websites 3: https://www.crunchyroll.com");
+console.log("Favorite Games 1: Legend of Zelda- all of them!");
+console.log("Favorite Games 2: Kingdom Hearts 2");
+console.log("Favorite Games 3: Portal and Portal 2");
+console.log("Favrite Artist, interactive web art:")
