eb5ce5f503a43d5a9473b41f364555df897f3e6f
Julia Ballas <42751526+jballas@users.noreply.github.com>
2020-02-15 13:26:41 -0700

    ARRAYs added
    I added 2 arrays, one with 3 images and one with 10 blanks

--- HW-5/script.js
+++ HW-5/script.js
@@ -1,4 +1,4 @@
-//A very Simple start, I made a function to change an image to another when you click on it.
+//A very Simple start, I made a function to change an image to another when you click on it. But it relies on IDs to change the images. How to I add IDs to my displayed images below???
 var img = "<img src='./images/placeholder-01.png'>";
 
 function flip_card(){
@@ -6,16 +6,37 @@ function flip_card(){
 }
 
 // Second goal is to make an array
+// I created a function with a for loop to display this array
 
-var blanks = ["<img src='./images/placeholder-01.png'>", "<img src='./images/placeholder-02.png'>"];
+function image_array(){
+    var images = ["<img src='./images/placeholder-01.png'>", "<img src='./images/placeholder-02.png'>", "<img src='./images/placeholder-03.png'>"];
+    var deck ="";
 
-  document.getElementById("deck").innerHTML = blanks;
-
-// for loop with this array
-/*
-function blank_array(){
-
-    for (var i = 0; i < blanks.length; i++ ){
-      blanks += blanks[i] + "<br>";
+    for (var i = 0; i < images.length; i++ ){
+      deck += images[i];
     }
-}*/
+    document.getElementById("deck").innerHTML = "Array Images" + deck;
+
+}
+
+// Third goal is to make an array with blank images
+// I used 2 for loops, one to create my blank deck with ten of the same image in it and one to display the images.
+
+function blank_array(){
+    var blanks = [];
+    var blank_card ="<img src='./images/blank.png'>";
+
+    //push the same image to empty array
+    var blank_deck="";
+    for (i = 0; i < 10; i++ ){
+      blanks.push(blank_card);
+    }
+
+    // display all the items in the array
+    for (i = 0; i < blanks.length; i++){
+      blank_deck += blanks[i] ;
+    }
+
+    document.getElementById("blank_deck").innerHTML = "Array with 10 blanks" + blank_deck;
+
+}
