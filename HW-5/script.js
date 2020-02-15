6deeb73fda94f80a1f43f39ea48e306a17417b27
Julia Ballas <42751526+jballas@users.noreply.github.com>
2020-02-15 12:59:42 -0700

    fixed images, updated script.js
    moved script from html to seperate file
    started adding array to the script

--- HW-5/script.js
+++ HW-5/script.js
@@ -1,5 +1,21 @@
-var image = "<img src='./images/placeholder-01.png'>";
+//A very Simple start, I made a function to change an image to another when you click on it.
+var img = "<img src='./images/placeholder-01.png'>";
 
 function flip_card(){
-  document.getElementById("first").onclick = image;
+  document.getElementById("first").innerHTML = img;
 }
+
+// Second goal is to make an array
+
+var blanks = ["<img src='./images/placeholder-01.png'>", "<img src='./images/placeholder-02.png'>"];
+
+  document.getElementById("deck").innerHTML = blanks;
+
+// for loop with this array
+/*
+function blank_array(){
+
+    for (var i = 0; i < blanks.length; i++ ){
+      blanks += blanks[i] + "<br>";
+    }
+}*/
