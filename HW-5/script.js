092417e7670cd5d5502b03fda2e7d9a8320c4e3f
Julia Ballas <42751526+jballas@users.noreply.github.com>
2020-02-15 22:21:34 -0700

    index and script updated
    I'm attempting to use my arrays to flip over blanks.

--- HW-5/script.js
+++ HW-5/script.js
@@ -1,26 +1,27 @@
-// image is not flipping anymore. WHY????
+// click on an image so it changes from blank
 
-var flipped = "<img src='./images/placeholder-01.png'>";
+var flipped = [];
 
 function flip_card(){
-  document.getElementById("second").innerHTML = flipped ;
-  console.log(flipped);
+  document.getElementById("new_card[10]").innerHTML = "TRYING" ;
+  console.log(deck);
 
 }
 
 
 // Second goal is to make an array
-// I created a function with a for loop to display this array
+// I created a function with a for loop to display this array, and now the display is random within the array, but it repeats numbers. do I have to split them off?
+//Or we tie the numbers to a specific location?
 
 function image_array(){
-    var images = ["<img src='./images/placeholder-01.png'>", "<img src='./images/placeholder-02.png'>", "<img src='./images/placeholder-03.png'>"];
+    var images = ["<img src='./images/placeholder-01.png'>", "<img src='./images/placeholder-02.png'>", "<img src='./images/placeholder-03.png'>", "<img src='./images/placeholder-04.png'>", "<img src='./images/placeholder-05.png'>", "<img src='./images/placeholder-06.png'>", "<img src='./images/placeholder-07.png'>", "<img src='./images/placeholder-08.png'>", "<img src='./images/placeholder-09.png'>", "<img src='./images/placeholder-10.png'>"];
     var deck ="";
 
-    for (var i = 0; i < images.length; i++ ){
-      deck += images[i];
+    for (var i = 0; i < 10; i++ ){
+      deck += images[Math.floor(Math.random() * 11)];
     }
     document.getElementById("deck").innerHTML = "Array Images" + deck;
-
+    console.log(deck);
 }
 
 // Third goal is to make an array with blank images
@@ -28,7 +29,8 @@ function image_array(){
 
 function blank_array(){
     var blanks = [];
-    var blank_card ="<img src='./images/blank.png' onclick='flip_card()'>";
+    var new_card = [];
+    var blank_card ="<img src='./images/blank.png' onclick='flip_card()' id='new_card[10]'>";
 
     //push the same image to empty array
     var blank_deck="";
