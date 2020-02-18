cc792817de63973574ae9e2043d453838406ee7e
Julia Ballas <42751526+jballas@users.noreply.github.com>
2020-02-17 20:53:44 -0700

    added Shuffle

--- HW-5/script.js
+++ HW-5/script.js
@@ -2,20 +2,45 @@
 var blank_card;
 
 //ARRAYS
-var images = ["<img src='./images/placeholder-01.png'>", "<img src='./images/placeholder-02.png'>", "<img src='./images/placeholder-03.png'>", "<img src='./images/placeholder-04.png'>", "<img src='./images/placeholder-05.png'>"];
+var images = ["<img src='./images/placeholder-01.png'>", "<img src='./images/placeholder-02.png'>", "<img src='./images/placeholder-03.png'>", "<img src='./images/placeholder-04.png'>", "<img src='./images/placeholder-05.png'>", "<img src='./images/placeholder-01.png'>", "<img src='./images/placeholder-02.png'>", "<img src='./images/placeholder-03.png'>", "<img src='./images/placeholder-04.png'>", "<img src='./images/placeholder-05.png'>"];
 
+// A loop that displays the blank image inside the
 function blank_array(){
 
       for (i = 0; i < 10; i ++){
           blank_card ="<img src='./images/blank.png' onclick='flip_card(" + i + ")'>";
-          console.log(blank_card);
+          //console.log(blank_card); // For testing purposes only so I can see what is in the blank card variable.
           document.getElementById("display" + i).innerHTML = blank_card;
       }
 
   }
 
+  // Source https://bost.ocks.org/mike/shuffle/
+  function shuffle(array) {
+    var m = array.length, t, i;
 
-    function flip_card(number){
+    // While there remain elements to shuffle…
+    while (m) {
 
-            document.getElementById("display"+number).innerHTML = images[Math.floor(Math.random() * 5)] ;
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+
+      // And swap it with the current element.
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+
+    return array;
+  }
+
+  shuffle(images);
+
+// Flips a blank card to an image, but it is tied to the IDs of the DIVs, and not the actual blank image.
+// How do I keep from repeating a random number? And yet still show that number twice?
+  function flip_card(number){
+
+    document.getElementById("display"+number).innerHTML = images[number];
+
 }
+//document.getElementById("display"+number).innerHTML = images[Math.floor(Math.random() * 5)]
