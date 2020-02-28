629dd31d2cf4223d43d58ea4056ec7b743dff622
Julia Ballas <42751526+jballas@users.noreply.github.com>
2020-02-27 20:10:59 -0700

    Update photoview.js

---
+++ HW-7/photoview.js
@@ -0,0 +1,18 @@
+// Photoviewer by Julia Ballas
+
+class Photo {
+
+    constructor(){
+      this.width = height
+      this.height = width
+
+    }
+
+    // method to switch between photos
+    switch(){
+
+    }
+
+    //
+
+}
