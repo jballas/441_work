32c5670a1c1527a4d0df2a251ba0875ae16afbe9
Julia Ballas <42751526+jballas@users.noreply.github.com>
2020-02-29 14:52:54 -0700

    Added new arrays
    Also updated the class constructors.
    Added new method to display images randomnly

--- HW-7/photoview.js
+++ HW-7/photoview.js
@@ -1,27 +1,58 @@
 // Photoviewer by Julia Ballas
 
+// ARRAYS
+var titles = new Array("title0", "title1", "title2", "title3", "title4");
+
+var authors = new Array("author0", "author1", "author2", "author3", "author4");
+
+var locations = new Array("loc0", "loc1", "loc2", "loc3", "loc4");
+
+var years = new Array("date0", "date1", "date2", "date3", "date4" );
+
+var descriptions = new Array("desc0", "des1", "des2", "des3", "des4" )
+
+var images = new Array("../HW-5/images/placeholder-06.png", "../HW-5/images/placeholder-07.png", "../HW-5/images/placeholder-08.png", "../HW-5/images/placeholder-09.png", "../HW-5/images/placeholder-10.png");
+
 class Photo {
 
     constructor(){
 
-    //  this.author;
-    //  this.location;
-      this.image = ["../HW-5/images/placeholder-02.png", "../HW-5/images/placeholder-02.png", "../HW-5/images/placeholder-03.png"];
-      this.random = Math.floor(Math.random() * this.image.length);
+      this.title = title;
+      this.author = author;
+      this.location = location;
+      this.year = year;
+    //  this.description = descrip;
+    //  this.src = src;
+      //this.random = Math.floor(Math.random() * this.images.length);
 
     }
 
     //method to display Photos, randomly
     toString(){
       let str;
-      str = this.image[this.random];
+      //str = this.image[this.random];
+      str = "Author: " + this.author + "<br> Location: " + this.location;
       return str;
     }
+
+      display(){
+
+        //for(let i = 0; i < images.length; i++){}
+        let random_num = Math.floor(Math.random() * images.length);
+
+
+            this.title = document.getElementById('title').innerHTML = titles[random_num]
+            this.src = document.getElementById('photo').src = images[random_num];
+            this.author = document.getElementById('author').innerHTML = authors[random_num];
+            this.location = document.getElementById('location').innerHTML = locations[random_num];
+            this.year = document.getElementById('year').innerHTML = years[random_num];
+            this.descript = document.getElementById('description').innerHTML = descriptions[random_num];
+
+      }
 
 
     // method to switch between photos
     /*switch(){
-      this.image[]
 
     }
 
@@ -29,19 +60,12 @@ class Photo {
 }
 
 // Create new photo and display it
-function display() {
-  let photo = new Photo();
+function new_photo() {
 
-  document.getElementById('photo').src = photo.toString();
+  let new_photo = new Photo();
+
+  new_photo.display();
+
 console.log(photo);
+
 }
-
-/*
-function switch_photo(){
-  let photo = new Photo();
-  photo.switch();
-
-} */
-
-display();
-//switch_photo();
