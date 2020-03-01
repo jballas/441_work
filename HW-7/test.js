8cd2a0cd6333e079aa5e28b64f526daa5f302d15
Julia Ballas <42751526+jballas@users.noreply.github.com>
2020-02-29 17:54:37 -0700

    updated test
    I switched around my test, it pushes the objects into an array and then displays the arrays randomly.

--- HW-7/test.js
+++ HW-7/test.js
@@ -3,65 +3,72 @@
 // ARRAY
 var photos = new Array();
 
-/*
-var titles = new Array("title0", "title1", "title2", "title3", "title4");
-
-var authors = new Array("author0", "author1", "author2", "author3", "author4");
-
-var locations = new Array("loc0", "loc1", "loc2", "loc3", "loc4");
-
-var years = new Array("date0", "date1", "date2", "date3", "date4" );
-
-var descriptions = new Array("desc0", "des1", "des2", "des3", "des4" )
-
-var images = new Array("../HW-5/images/placeholder-06.png", "../HW-5/images/placeholder-07.png", "../HW-5/images/placeholder-08.png", "../HW-5/images/placeholder-09.png", "../HW-5/images/placeholder-10.png");
-*/
-
 class Photo {
 
-    constructor(){
+    constructor(title, author, location, date, des, image){
 
-      this.title =  new Array("title0", "title1", "title2", "title3", "title4");
-      this.author = new Array("author0", "author1", "author2", "author3", "author4");
-      this.location = new Array("loc0", "loc1", "loc2", "loc3", "loc4");
-      this.year = new Array("date0", "date1", "date2", "date3", "date4" );
-      this.description = new Array("desc0", "des1", "des2", "des3", "des4" )
-      this.image = new Array("../HW-5/images/placeholder-06.png", "../HW-5/images/placeholder-07.png", "../HW-5/images/placeholder-08.png", "../HW-5/images/placeholder-09.png", "../HW-5/images/placeholder-10.png");
-
-      this.display();
+      this.title =  title;
+      this.author = author;
+      this.location = location;
+      this.year = date;
+      this.description = des;
+      this.image = image;
     }
 
-    //method to display Photos
-      display(){
+    // method to display Photos
+      toString(){
 
-        for (let i = 0; i < 5; i++){
-          this.title = document.getElementById('title').innerHTML = this.title[i]
-
-          this.author = document.getElementById('author').innerHTML = this.author[i];
-
-          this.location = document.getElementById('location').innerHTML = this.location[i];
-          this.year = document.getElementById('year').innerHTML = this.year[i];
-          this.description = document.getElementById('description').innerHTML = this.description[i];
-          this.image = document.getElementById('photo').src = this.image[i];
-
-          photos.push(new Photo())
-        }
-      //  let random_num = Math.floor(Math.random() * this.image.length);
-
-
-
+        let str;
+        str = "Title: " + this.title + "<br> Author: " + this.author + "<br> Location: " + this.location + "<br> Year: " + this.year + "<br> Description: " + this.description +"<br>"
+        return str;
       }
+
+      get details(){
+        return "Title: " + this.title + "<br> Author: " + this.author + "<br> Location: " + this.location + "<br> Year: " + this.year + "<br> Description: " + this.description +"<br>";
+      }
+
+      display_image(){
+        document.getElementById('photo').src = this.image
+      }
+  }
+
+/*
+          document.getElementById('author').innerHTML = this.author[i];
+
+          document.getElementById('location').innerHTML = this.location[i];
+
+          document.getElementById('year').innerHTML = this.year[i];
+
+          document.getElementById('description').innerHTML = this.description[i];
+
+          document.getElementById('photo').src = this.image[i];
+*/
+
+// IMAGES SRC
+/*new Array("../HW-5/images/placeholder-06.png", "../HW-5/images/placeholder-07.png", "../HW-5/images/placeholder-08.png", "../HW-5/images/placeholder-09.png", "../HW-5/images/placeholder-10.png");*/
+
+
+function display(){
+//Create a new object with following parameters (title, author, location, date, des, image)
+  let new_photo = new Photo("Montana Sunset","Julia Ballas","Montana", "2020", "A sunset showing off the Spanish Peaks in Central Montana", "../HW-5/images/placeholder-06.png" );
+
+ let second_photo = new Photo("City Sunset","Julia Ballas","New York", "2020", "A sunset showing off New York", "../HW-5/images/placeholder-07.png" );
+
+// push them into an array
+  photos.push(new_photo);
+  photos.push(second_photo);
 
 }
 
 // Create new photo and display it
 function new_photo() {
 
+  // But this continues to push them into the array???
 
 
+  let random_num = Math.floor(Math.random() * 2);
 
-//  photos.display();
-
-console.log(photos);
-
+  document.getElementById('title').innerHTML = photos[random_num];
+  photos[random_num].display_image();
+  console.log(random_num);
 }
