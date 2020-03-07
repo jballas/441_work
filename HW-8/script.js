fa1e1e3f768a32cd0b03f46d788fad257cbc4d8c
Julia Ballas <42751526+jballas@users.noreply.github.com>
2020-03-07 10:23:25 -0700

    Update script.js
    put the stars back in, attempted to use mouseenter(), but it created a weird bug.

--- HW-8/script.js
+++ HW-8/script.js
@@ -1,17 +1,19 @@
 $(document).ready(function(){
 
 // stars
+//$('#screen').mouseenter(function(){
+  $('#star2').delay(1000).fadeOut("slow").fadeIn("slow");
+  $('#star3').delay(1200).fadeOut("slow").fadeIn("slow");
+  $('#star4').delay(1400).fadeOut("slow").fadeIn("slow");
+  $('#star5').delay(1600).fadeOut("slow").fadeIn("slow");
+  $('#star6').delay(1800).fadeOut("slow").fadeIn("slow");
+  $('#star7').delay(2000).fadeOut("slow").fadeIn("slow");
+  $('#star8').delay(2200).fadeOut("slow").fadeIn("slow");
+  $('#star9').delay(2400).fadeOut("slow").fadeIn("slow");
+  $('#star1').delay(800).fadeOut("slow").fadeIn("slow");
+  $('#star10').delay(2600).fadeOut("slow").fadeIn("slow");
 
-      $('#star1').delay(800).fadeOut("slow").fadeIn("slow");
-      $('#star2').delay(1000).fadeOut("slow").fadeIn("slow");
-      $('#star3').delay(1200).fadeOut("slow").fadeIn("slow");
-      $('#star4').delay(1400).fadeOut("slow").fadeIn("slow");
-      $('#star5').delay(1600).fadeOut("slow").fadeIn("slow");
-      $('#star6').delay(1800).fadeOut("slow").fadeIn("slow");
-      $('#star7').delay(2000).fadeOut("slow").fadeIn("slow");
-      $('#star8').delay(2200).fadeOut("slow").fadeIn("slow");
-      $('#star9').delay(2400).fadeOut("slow").fadeIn("slow");
-      $('#star10').delay(2600).fadeOut("slow").fadeIn("slow");
+//});
 
 
 
@@ -22,10 +24,10 @@ $(document).ready(function(){
 
             // Movie begins
               $('#title_card').text("Watch out!").delay(500).fadeOut("slow", function(){
-                $('#title_card2').text("Stop Thief!").delay(500).fadeOut("slow", function(){
+                $('#movie_image').attr("src", "./images/mary-pickford.jpg").animate({width: "400px"}).fadeOut(2000, function(){
 
                   // image 1 sequence
-                  $('#movie_image').attr("src", "./images/mary-pickford.jpg").animate({width: "400px"}).fadeOut(2000, function(){
+                  $('#title_card2').text("Stop Thief!").delay(500).fadeOut("slow", function(){
                       $('#movie_image').fadeIn().attr("src", "./images/john-gilbert.jpg").delay(1500).animate({width: "150px", top: "150px", right: "150px"}).delay(2000, function(){
                         $('#title_card').fadeIn().text("My Hero!").delay(1500);
 
