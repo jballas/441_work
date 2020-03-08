1de6a916662d453dae3ddc9d23d8d182552dfc9a
Julia Ballas <42751526+jballas@users.noreply.github.com>
2020-03-07 19:33:34 -0700

    created hidden about div

--- HW-8/script.js
+++ HW-8/script.js
@@ -1,34 +1,42 @@
-var movie_stars = new Array('./images/mary-pickford.jpg', './images/buster-keaton.jpg', './images/chaplin, jpg', './images/john-gilbert.jpg', './images/dancer.jpg');
+var movie_stars = new Array('./images/mary-pickford.jpg', './images/buster-keaton.jpg', './images/chaplin, jpg', './images/john-gilbert.jpg', './images/dancer.gif');
 
 
 $(document).ready(function(){
 
-// stars
+stars();
+// stars function will run again after a delay
+setInterval(function(){   stars()  }, 4000);
 
-   $('#star2').delay(1000).fadeOut("slow").fadeIn("slow");
-   $('#star3').delay(1200).fadeOut("slow").fadeIn("slow");
-   $('#star4').delay(1400).fadeOut("slow").fadeIn("slow");
-   $('#star5').delay(1600).fadeOut("slow").fadeIn("slow");
-   $('#star6').delay(1800).fadeOut("slow").fadeIn("slow");
-   $('#star7').delay(2000).fadeOut("slow").fadeIn("slow");
-   $('#star8').delay(2200).fadeOut("slow").fadeIn("slow");
-   $('#star9').delay(2400).fadeOut("slow").fadeIn("slow");
-   $('#star1').delay(800).fadeOut("slow").fadeIn("slow");
-   $('#star10').delay(2600).fadeOut("slow").fadeIn("slow");
+function stars(){
+    $('#star2').delay(1000).fadeOut("slow").fadeIn("slow");
+    $('#star3').delay(1200).fadeOut("slow").fadeIn("slow");
+    $('#star4').delay(1400).fadeOut("slow").fadeIn("slow");
+    $('#star5').delay(1600).fadeOut("slow").fadeIn("slow");
+    $('#star6').delay(1800).fadeOut("slow").fadeIn("slow");
+    $('#star7').delay(2000).fadeOut("slow").fadeIn("slow");
+    $('#star8').delay(2200).fadeOut("slow").fadeIn("slow");
+    $('#star9').delay(2400).fadeOut("slow").fadeIn("slow");
+    $('#star1').delay(800).fadeOut("slow").fadeIn("slow");
+    $('#star10').delay(2600).fadeOut("slow").fadeIn("slow");
+}
+
+// Display About using jQuery slideToggle()
+
+  $('#btn_about').click(function(){
+    $('#about').slideToggle();
+    })
 
 
-
-
-
-//the star are strange if you hide the button before the stars finishing their fadding.
-      $('button').click(function(){
+//When you click the button, the 'movie' begins
+      $('#btn_play').click(function(){
         $('.star').hide( function(){
-          $('button').hide( function(){
+          $('#btn_play').hide( function(){
 
             let random_image = movie_stars[Math.floor(Math.random() * movie_stars.length)];
             console.log(random_image)
 
             // Movie begins
+            $('#title').text("Amazing! Bizarre! Adventures Begin Now!");
               $('#title_card').text("Watch out!").delay(500).fadeOut("slow", function(){
                 $('#movie_image').attr("src", random_image).animate({width: "400px"}).fadeOut(2000, function(){
 
