bef7de61e6a3fea0a9a9fd312f1be555c38ff405
Julia Ballas <42751526+jballas@users.noreply.github.com>
2020-03-11 21:02:18 -0600

    Update script.js

--- HW-9/script.js
+++ HW-9/script.js
@@ -1,28 +1,32 @@
 $(document).ready(function(){
   $("#btn_info").click(function(){
 
-  /*$.getJSON("https://jballas.github.io/441_work/HW-9/data/small.json", function(data){
+  $.getJSON("https://jballas.github.io/441_work/HW-9/data/small.json", function(data){
         console.log("success");
         $.each(data, function(i, field){
           $("#show_information").append(field + " ");
         });
        });
+
+       var jqxhr = $.getJSON( "https://jballas.github.io/441_work/HW-9/data/small.json", function() {
+         console.log( "success" );
+       })
+         .done(function() {
+           console.log( "second success" );
+         })
+         .fail(function() {
+           console.log( "error" );
+         })
+         .always(function() {
+           console.log( "complete" );
+         });
+
     });
 $("#button2").click(function(){
   $("#show_information").toggle();
 })
-*/
-var jqxhr = $.getJSON( "https://jballas.github.io/441_work/HW-9/data/small.json", function() {
-  console.log( "success" );
-})
-  .done(function() {
-    console.log( "second success" );
-  })
-  .fail(function() {
-    console.log( "error" );
-  })
-  .always(function() {
-    console.log( "complete" );
-  });
-});
+
+
+
+
 })
