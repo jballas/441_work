068e297393d07e961e0b3b8ec4c6de2bb81f152d
Julia Ballas <42751526+jballas@users.noreply.github.com>
2020-03-11 19:56:37 -0600

    Update script.js

--- HW-9/script.js
+++ HW-9/script.js
@@ -1,11 +1,14 @@
 $(document).ready(function(){
   $("button").click(function(){
-    $.getJSON("https://jballas.github.io/441_work/HW-9/data/small.json", function(data){
-      $("#show_information").append(data);
-    });
+    $.getJSON("https://jballas.github.io/441_work/HW-9/data/small.json", function(result){
+      $.each(result, function(i, field){
+        $("#show_information").append(field + " ");
+      });
+        });
+      });
+
 //  $("#show_information").hide();
 
 
-  })
 
 })
