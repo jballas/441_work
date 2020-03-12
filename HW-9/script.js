f53caf3d0c8cf5901a1f765f5e43fd571b7e4097
Julia Ballas <42751526+jballas@users.noreply.github.com>
2020-03-11 20:36:59 -0600

    Update script.js
    trying to add console logs to see if this will even work

--- HW-9/script.js
+++ HW-9/script.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
   $("#btn_info").click(function(){
     $.getJSON("https://jballas.github.io/441_work/HW-9/data/small.json", function(data){
+      console.log("success");
       $.each(data, function(i, field){
         $("#show_information").append(field + " ");
       });
      });
     });
-
 $("#button2").click(function(){
   $("#show_information").toggle();
 })
