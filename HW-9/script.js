404e6d1478211783f4a1b55d56a58a5787c7fcdd
Julia Ballas <42751526+jballas@users.noreply.github.com>
2020-03-11 20:47:22 -0600

    Update script.js
    second attempt to track using console.log

--- HW-9/script.js
+++ HW-9/script.js
@@ -1,16 +1,28 @@
 $(document).ready(function(){
   $("#btn_info").click(function(){
-    $.getJSON("https://jballas.github.io/441_work/HW-9/data/small.json", function(data){
-      console.log("success");
-      $.each(data, function(i, field){
-        $("#show_information").append(field + " ");
-      });
-     });
+
+  /*$.getJSON("https://jballas.github.io/441_work/HW-9/data/small.json", function(data){
+        console.log("success");
+        $.each(data, function(i, field){
+          $("#show_information").append(field + " ");
+        });
+       });
     });
 $("#button2").click(function(){
   $("#show_information").toggle();
 })
-
-
-
+*/
+var jqxhr = $.getJSON( "https://jballas.github.io/441_work/HW-9/data/small.json", function() {
+  console.log( "success" );
+})
+  .done(function() {
+    console.log( "second success" );
+  })
+  .fail(function() {
+    console.log( "error" );
+  })
+  .always(function() {
+    console.log( "complete" );
+  });
+});
 })
