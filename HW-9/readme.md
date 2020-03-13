56cdc6d69f9e297678536c5c59caae1042beaa06
Julia Ballas <42751526+jballas@users.noreply.github.com>
2020-03-12 19:13:02 -0600

    added new class Rock
    added references where I'm getting help from. Added new divs to display information

--- HW-9/readme.md
+++ HW-9/readme.md
@@ -23,6 +23,15 @@ Databases. This week we are collecting information from a datasource and display
 
 As recommend I started with taking a small json file and trying to display it. I ran straight into a issue with 'cross-origin-request error'. Despite the fact I was using Firefox, it needed my json to be a secure link, so I hard coded to by github source. Only it still didn't work because my I'd edited my json file and left out the end of a bracket. So, after struggling and debugging for a while with console-log, copied straight from Jquery's website, and using another json file, I finally got my display, which consisted of [object Object][object Object], which is not meaningful yet.
 
+
+references
+https://www.w3schools.com/jquery/ajax_getjson.asp
+https://api.jquery.com/jQuery.getJSON/
+https://api.jquery.com/jquery.each/
+
+To only display part of a string, I used the `.substr(0,4)`, which I discovered here:
+https://stackoverflow.com/questions/3414916/display-only-10-characters-of-a-long-string
+
 ## Problems or Issues this week
 
 ## Specific questions/concerns for next week
--- HW-9/script.js
+++ HW-9/script.js
@@ -1,10 +1,20 @@
 $(document).ready(function(){
+
+
   $("#btn_info").click(function(){
 
   $.getJSON("https://jballas.github.io/441_work/HW-9/data/small.json", function(data){
 
         $.each(data, function(i, field){
-          $("#show_information").append("Name: " + field.name + "Mass: " + field.mass + "<br>");
+
+        //  $("#show_information").append("Name: " + field.name + " Mass: " + field.mass + " Year: " + field.year.substr(0,4) + " Geolocation: " + field.geolocation.coordinates + "<br>" );
+
+          $("#name").text("Name: " + field.name);
+          $("#mass").text(" Mass: " + field.mass);
+          $("#year").text(" Year: " + field.year.substr(0,4));
+          $("#geolocation").text(" Geolocation: " + field.geolocation.coordinates);
+
+
         });
        });
 
@@ -12,6 +22,27 @@ $(document).ready(function(){
 $("#button2").click(function(){
   $("#show_information").toggle();
 })
+
+class Rock {
+
+  constructor(name, mass, year, location){
+
+    this.name = name;
+    this.mass = mass;
+    this.year = year;
+    this.location = location;
+
+    toString(){
+      let str;
+      str = "Name: " + this.name + " Mass: " + this.mass + " Year: " + this.year.substr(0,4) + " Geolocation: " + this.location + "<br>";
+      return str;
+    }
+
+    get details(){
+      return "Name: " + this.name + " Mass: " + this.mass + " Year: " + this.year.substr(0,4) + " Geolocation: " + this.location + "<br>";
+    }
+  }
+}
 
 
 
