0ea9a73a9010ee85fd48a945570815fb7d36a7d8
Julia Ballas <42751526+jballas@users.noreply.github.com>
2020-03-22 21:33:55 -0600

    updated dots plugin
    i experimented with my dots.js, adding a class called Circle.
    updated style
    added css needed to make the dots appear correctly.

--- HW-9/dots.js
+++ HW-9/dots.js
@@ -7,13 +7,54 @@ $(document).ready(function(){
 
   $.fn.dots = function(){
     var circle = "<svg height='5' width='5'><circle cx='2' cy='2' r='2' fill='black'/></svg>"
+
     for (i = 0; i < 50 ; i ++){
         this.append(circle);
+        this.css("height", "10px")
+        this.css("overflow", "hidden")
     }
     return this;
   };
 }(jQuery));
 
+
+// Let's create a Class for the svg_circle
+// Unfortunately, I can't seem to connect it to the plugin...
+
+class Circle {
+  constructor (spacing, cx, cy, radius, fill){
+    this.height = cy*2;
+    this.width = spacing;
+    this.cx = cx;
+    this.cy = cy;
+    this.radius = radius;
+    this.fill = fill;
+  }
+
+  toString(){
+    let str;
+    str = "<svg height='" + this.height + "' width='" + this.width + "'><circle cx='" + this.cx + "' cy='" + this.cy + "' r='"+ this.radius + "' fill='" + this.fill + "'/></svg>"
+    return str;
+  }
+
+
+  get details(){
+    return this.height + " " + this.width + " " + this.cx + " " + this.cy + " " + this.radius + " " + this.fill;
+
+  }
+
+}
+
+// Testing that the class works, and displays different circles
+var small_circle = new Circle(10,2,2,5, "pink")
+var big_circle = new Circle(100, 150, 150, 150, "red");
+var display = small_circle.toString();
+
+
+  $("#test_button").click( function(){
+    $("#trial").html(display).dots();
+
+  })
 
 
 })
