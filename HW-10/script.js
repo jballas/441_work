7bf477c126492d44dfe2533a104155e55b3691a5
Julia Ballas <42751526+jballas@users.noreply.github.com>
2020-03-28 12:34:09 -0600

    Update script.js

--- HW-10/script.js
+++ HW-10/script.js
@@ -6,8 +6,8 @@ $(document).ready(function(){
   class Bug{
 
       constructor (x, y){
-        this.width = canvas.width;
-        this.height = canvas.height;
+        this.width = 50;
+        this.height = 50;
         this.x = x;
         this.y = y;
       }
@@ -18,38 +18,46 @@ $(document).ready(function(){
 
       create_square(){
         ctx.fillStyle = "rgb(22, 175, 195)";
-        ctx.fillRect(this.x,this.y,50,50);
+        ctx.fillRect(this.x,this.y,this.width,this.height);
       }
 
   }
+//  let blue_bug = new Bug(x, y);
+//  blue_bug.create_square();
 
-//
-  draw_square();
-  setInterval(update,1000/60);
+var width = 50;
+var height = 50;
+var x = canvas.width/2;
+var y = canvas.height/2;
+ctx.fillStyle = "rgb(22, 175, 195)";
+
+// creates a new bug from class
+function draw_square(){
+
+    ctx.fillRect(x,y,width,height);
+      x += -1;
+      y += 1;
+  if (x == canvas.width || y == canvas.height || x == 0 || y == 0){
+    x=canvas.width / 2;
+    y=canvas.width / 2;
+  }
+}
+
+draw_square();
+setInterval(update,1000/60);
 
   function update(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
     draw_square();
   }
 
-function draw_square(){
-  let x = canvas.height/2;
-  let y = canvas.width/2
+})
 
-  let blue_bug = new Bug(x, y);
-  blue_bug.create_square();
 
-    let random_num = Math.floor(Math.random() * 2);
+/*    let random_num = Math.floor(Math.random() * 2);
 
     x += random_num ;
     y += random_num ;
 
-    if (x == canvas.width || y == canvas.height || x == 0 || y == 0){
-      x=canvas.width / 2;
-      y=canvas.width / 2;
-    }
-}
 
-
-
-})
+*/
