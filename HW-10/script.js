1f0a818c245f25fd301669a834c39a7bf54e6ba3
Julia Ballas <42751526+jballas@users.noreply.github.com>
2020-03-29 13:44:49 -0600

    Update script.js
    I have 1 class, but can't get them to move with WSAD button so I can check for collision

--- HW-10/script.js
+++ HW-10/script.js
@@ -11,6 +11,9 @@ $(document).ready(function(){
         this.x = x;
         this.y = y;
         this.fill = fill;
+        this.control ;
+        this.direction = 1;
+        this.speed = 20;
       }
 
       get details(){
@@ -20,45 +23,90 @@ $(document).ready(function(){
       create_square(){
         ctx.fillStyle = this.fill;
         ctx.fillRect(this.x,this.y,this.width,this.height);
+        this.move();
       }
 
-  }
+      move(){
+
+        if (this.control = 1){
+          $('canvas').css("background-color", "red");
+          }
+          $(this).keypress(function(event){
+            getKey(event);
+          });
+
+         if (this.control=2){
+              /*
+          // Move squares across the screen
+          // example taken from https://p5js.org/examples/motion-bounce.html
+          this.x = this.x + this.direction;
+          this.y = this.y + this.direction;
+          // wrap screen, so square reappers
+          if (this.x >= canvas.width || y >= canvas.height || this.x == 0 || this.y == 0){
+            this.direction *= -1;
+          }*/
+        }
+      }
+
+      getKey(event){
+
+       var char = event.which || event.keyCode;
+       var actualLetter = String.fromCharCode(char);
+       console.log(actualLetter);
+
+       if (actualLetter == "w"){
+         this.y -= speed;
+         console.log(this.y)
+       }
+
+       if (actualLetter =="s"){
+         this.y += speed;
+       }
+
+       if (actualLetter == "a"){
+         this.x -= speed;
+       }
+
+       if (actualLetter == "d"){
+         this.x += speed;
+       }
+     }
+   }
 
 // Variables
 player_x =0;
 player_y = canvas.height - 100;
 var x = canvas.width/2;
 var y = canvas.height/2;
-var direction = 1;
 
 
-// creates a new bug from class
-function draw_square(){
+// create 2 objects
+var player_bug = new Bug(player_x, player_y, 50, 100, "rgb(100, 31, 196)");
+var blue_bug = new Bug(x, y, 10, 10, "rgb(22, 175, 195)");
 
-  let blue_bug = new Bug(x, y, 10, 10, "rgb(22, 175, 195)");
-  blue_bug.create_square();
-
-
+/*
+move(){
   // Move square across the screen
-  // https://p5js.org/examples/motion-bounce.html
-    x = x + direction;
-    y = y + direction;
+  // example taken from https://p5js.org/examples/motion-bounce.html
+  x = x + direction;
+  y = y + direction;
+  console.log(x)
+}*/
 
 
-  // wrap screen, so square reappers
-  if (x >= canvas.width || y >= canvas.height || x == 0 || y == 0){
-    direction *= -1;
-  }
+
+// draws a new bug from object we created above
+function draw_square(){
+  blue_bug.create_square();
+  blue_bug.control = 1;
 }
+
 
 // Create second square from class.
 
 function player_square(){
-
-
-  let player_bug = new Bug(player_x, player_y, 50, 100, "rgb(100, 31, 196)");
   player_bug.create_square();
-
+  player_bug.control = 1;
 }
 
 // Call functions
@@ -70,12 +118,14 @@ setInterval(update,1000/60);
     ctx.clearRect(0,0,canvas.width,canvas.height);
     draw_square();
     player_square();
+    if (boxes_collide(blue_bug,player_bug)){
+      window.alert("bingo");
+    }
 
   }
 
-//boxes_collide(player_bug, blue_bug);
 
-// Move player draw_square
+/* Move player draw_square
 $(this).keypress(function(event){
   getKey(event);
 });
@@ -84,15 +134,28 @@ $(this).keypress(function(event){
 // Check if 'wsad' is pressed
 function getKey(event){
 
+  let speed = 20;
   var char = event.which || event.keyCode;
   var actualLetter = String.fromCharCode(char);
 
   if (actualLetter == "w"){
-    player_y -=10;
+    player_y -= speed;
     console.log(player_y)
   }
-}
 
+  if (actualLetter =="s"){
+    player_y += speed;
+  }
+
+  if (actualLetter == "a"){
+    player_x -= speed;
+  }
+
+
+  if (actualLetter == "d"){
+    player_x += speed;
+  }
+}*/
 
 // Collisions
 
