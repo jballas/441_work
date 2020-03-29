b404c38ada5aadeb49c4a9503d887593c0d602cd
Julia Ballas <42751526+jballas@users.noreply.github.com>
2020-03-29 15:02:28 -0600

    Update script.js

--- HW-10/script.js
+++ HW-10/script.js
@@ -11,66 +11,23 @@ $(document).ready(function(){
         this.x = x;
         this.y = y;
         this.fill = fill;
-        this.control ;
-        this.direction = 1;
-        this.speed = 20;
+
       }
 
-      get details(){
-        return this.x + this.y + this.width + this.height + this.fill;
+      get width(){
+        return this.width;
+      }
+
+      set width(){
+        this.width = width;
       }
 
       create_square(){
         ctx.fillStyle = this.fill;
         ctx.fillRect(this.x,this.y,this.width,this.height);
-        this.move();
       }
 
-      move(){
 
-        if (this.control = 1){
-          $('canvas').css("background-color", "red");
-          }
-          $(this).keypress(function(event){
-            getKey(event);
-          });
-
-         if (this.control=2){
-              /*
-          // Move squares across the screen
-          // example taken from https://p5js.org/examples/motion-bounce.html
-          this.x = this.x + this.direction;
-          this.y = this.y + this.direction;
-          // wrap screen, so square reappers
-          if (this.x >= canvas.width || y >= canvas.height || this.x == 0 || this.y == 0){
-            this.direction *= -1;
-          }*/
-        }
-      }
-
-      getKey(event){
-
-       var char = event.which || event.keyCode;
-       var actualLetter = String.fromCharCode(char);
-       console.log(actualLetter);
-
-       if (actualLetter == "w"){
-         this.y -= speed;
-         console.log(this.y)
-       }
-
-       if (actualLetter =="s"){
-         this.y += speed;
-       }
-
-       if (actualLetter == "a"){
-         this.x -= speed;
-       }
-
-       if (actualLetter == "d"){
-         this.x += speed;
-       }
-     }
    }
 
 // Variables
@@ -78,35 +35,35 @@ player_x =0;
 player_y = canvas.height - 100;
 var x = canvas.width/2;
 var y = canvas.height/2;
-
+var direction = 1;
 
 // create 2 objects
 var player_bug = new Bug(player_x, player_y, 50, 100, "rgb(100, 31, 196)");
 var blue_bug = new Bug(x, y, 10, 10, "rgb(22, 175, 195)");
 
-/*
-move(){
-  // Move square across the screen
-  // example taken from https://p5js.org/examples/motion-bounce.html
-  x = x + direction;
-  y = y + direction;
-  console.log(x)
-}*/
+// Move square across the screen
+// example taken from https://p5js.org/examples/motion-bounce.html
+
+function move(object1){
+  object1.x = object1.x + direction;
+  object1.y = object1.y + direction;
 
 
+  if (object1.x >= canvas.width || y >= canvas.height || blue_bug.x == 0 || object1.y == 0){
+           direction *= -1;
+         }
+}
 
 // draws a new bug from object we created above
 function draw_square(){
   blue_bug.create_square();
-  blue_bug.control = 1;
+  move(blue_bug);
 }
-
 
 // Create second square from class.
 
 function player_square(){
   player_bug.create_square();
-  player_bug.control = 1;
 }
 
 // Call functions
@@ -118,14 +75,16 @@ setInterval(update,1000/60);
     ctx.clearRect(0,0,canvas.width,canvas.height);
     draw_square();
     player_square();
+
     if (boxes_collide(blue_bug,player_bug)){
-      window.alert("bingo");
+      console.log('boxes collided!')
     }
 
   }
 
 
-/* Move player draw_square
+// Move player draw_square
+
 $(this).keypress(function(event){
   getKey(event);
 });
@@ -139,23 +98,23 @@ function getKey(event){
   var actualLetter = String.fromCharCode(char);
 
   if (actualLetter == "w"){
-    player_y -= speed;
+    player_bug.y -= speed;
     console.log(player_y)
   }
 
   if (actualLetter =="s"){
-    player_y += speed;
+    player_bug.y += speed;
   }
 
   if (actualLetter == "a"){
-    player_x -= speed;
+    player_bug.x -= speed;
   }
 
 
   if (actualLetter == "d"){
-    player_x += speed;
+    player_bug.x += speed;
   }
-}*/
+}
 
 // Collisions
 
