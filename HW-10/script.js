640ca38fe47baf5d29d20d9a9071e449e5e1bc63
Julia Ballas <42751526+jballas@users.noreply.github.com>
2020-03-28 19:01:26 -0600

    updated so square bounces
    You can move the square with w
    but collisions don't work

--- HW-10/script.js
+++ HW-10/script.js
@@ -5,58 +5,112 @@ $(document).ready(function(){
 
   class Bug{
 
-      constructor (x, y){
-        this.width = 50;
-        this.height = 50;
+      constructor (x, y, width, height, fill){
+        this.width = width;
+        this.height = height;
         this.x = x;
         this.y = y;
+        this.fill = fill;
       }
 
       get details(){
-        return this.x + this.y + this.width + this.height;
+        return this.x + this.y + this.width + this.height + this.fill;
       }
 
       create_square(){
-        ctx.fillStyle = "rgb(22, 175, 195)";
+        ctx.fillStyle = this.fill;
         ctx.fillRect(this.x,this.y,this.width,this.height);
       }
 
   }
 
-
-var width = 50;
-var height = 50;
+// Variables
+player_x =0;
+player_y = canvas.height - 100;
 var x = canvas.width/2;
 var y = canvas.height/2;
-ctx.fillStyle = "rgb(22, 175, 195)";
+var direction = 1;
+
 
 // creates a new bug from class
 function draw_square(){
 
-  let blue_bug = new Bug(x, y);
+  let blue_bug = new Bug(x, y, 10, 10, "rgb(22, 175, 195)");
   blue_bug.create_square();
-      x += 1;
-      y += 1;
 
-  if (x == canvas.width || y == canvas.height || x == 0 || y == 0){
-    x=0;
-    y=0;
+
+  // Move square across the screen
+    x = x + direction;
+    y = y + direction;
+
+
+  // wrap screen, so square reappers
+  if (x >= canvas.width || y >= canvas.height || x == 0 || y == 0){
+    direction *= -1;
   }
 }
 
+// Create second square from class.
+
+function player_square(){
+
+
+  let player_bug = new Bug(player_x, player_y, 50, 100, "rgb(100, 31, 196)");
+  player_bug.create_square();
+
+}
+
+// Call functions
 draw_square();
+player_square();
 setInterval(update,1000/60);
 
   function update(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
     draw_square();
+    player_square();
+
   }
+
+//boxes_collide(player_bug, blue_bug);
+
+// Move player draw_square
+$(this).keypress(function(event){
+  getKey(event);
+});
+
+
+// Check if 'wsad' is pressed
+function getKey(event){
+
+  var char = event.which || event.keyCode;
+  var actualLetter = String.fromCharCode(char);
+
+  if (actualLetter == "w"){
+    player_y -=10;
+    console.log(player_y)
+  }
+}
+
+
+// Collisions
+
+function boxes_collide(object1,object2){
+  return !(
+    ((object1.y + object1.height) < (object2.y)) ||
+    (object2.y > (object2.y + object2.height)) ||
+    ((object1.x + object1.width) < object2.x) ||
+    (object1.x + (object2.y + object2.width))
+  );
+}
+
 
 })
 
+/// Creates random_number variable
+/*    let random_num = Math.floor(Math.random() * canvas.width);
 
-/*    let random_num = Math.floor(Math.random() * 2);
-
+  console.log(random_num);
     x += random_num ;
     y += random_num ;
 
