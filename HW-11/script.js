3eb818b80a07d800e4173911f029e9af9786b83b
Julia Ballas <42751526+jballas@users.noreply.github.com>
2020-04-03 14:56:45 -0600

    Update script.js
    I can move my players and switch between them

--- HW-11/script.js
+++ HW-11/script.js
@@ -1,28 +1,29 @@
 $(document).ready(function(){
 
-var squares =  {"info":[{"x" : "10",
-                         "y" : "0",
-                         "width" : "5",
-                         "height" : "5",
+var squares =  {"info":[{"x" : 10,
+                         "y" : 0,
+                         "width" : 5,
+                         "height" : 5,
                          "color" : "rgb(228, 6, 231)"},
-           {"x" : "10",
-            "y" : "10",
-            "width" : "5",
-            "height" : "5",
+           {"x" : 10,
+            "y" : 10,
+            "width" : 5,
+            "height" : 5,
             "color" : "rgb(35, 110, 37)"},
-            {"x" : "10",
-             "y" : "20",
-             "width" : "5",
-             "height" : "5",
+            {"x" : 10,
+             "y" : 20,
+             "width" : 5,
+             "height" : 5,
              "color" : "rgb(209, 177, 23)"},
-             {"x" : "10",
-              "y" : "30",
-              "width" : "5",
-              "height" : "5",
+             {"x" : 10,
+              "y" : 30,
+              "width" : 5,
+              "height" : 5,
               "color" : "rgb(223, 148, 38)"}]};
 
 
 var squid = new Array();
+var player = 0;
 
 class Fish {
 
@@ -33,6 +34,23 @@ class Fish {
     this.height = height;
     this.color = color;
   }
+
+  get fishx(){
+    return this.x;
+  }
+
+  set fishx(x){
+    this.x = x;
+  }
+
+  get fishy(){
+    return this.y;
+  }
+
+  set fishy(y){
+    this.y = y;
+  }
+
 
   toString(){
     let str;
@@ -60,49 +78,62 @@ class Fish {
     let j = new Fish(squares.info[i].x, squares.info[i].y, squares.info[i].width, squares.info[i].height, squares.info[i].color
     );
     squid.push(j);
-    console.log(j);
+    //console.log(j)
   }
 
     }
 
     create_squid();
      drawSquare();
+     setInterval(update, 1000/60);
 
-     function update()
-     {
-
+     function update() {
+       ctx.clearRect(0,0,canvas.width,canvas.height);
          drawSquare();
      }
 
-     function drawSquare()
-     {
-       for ( let i = 0; i < 4; i ++){
+     function drawSquare(){
+       for ( let i = 0; i < squid.length ; i ++){
          ctx.fillStyle = squid[i].color;
         ctx.fillRect(squid[i].x, squid[i].y, squid[i].width, squid[i].height);
-
        }
-
      }
-
 
 
 // KEY EVENT
      $(this).keypress(function(event){
        getKey(event);
+       console.log(squid[player].x)
+       console.log(player)
      });
 
      function getKey(event){
+
+       var speed = 10;
        var char = event.which || event.keyCode;
        var letter = String.fromCharCode(char);
 
        if(letter == "w"){
-         moveUp();
+         squid[player].y -= speed;
        }
-       drawSquare();
+       if (letter == "s"){
+         squid[player].y += speed;
+       }
+       if (letter == "a"){
+         squid[player].x -= speed;
+       }
+       if (letter == "d"){
+         squid[player].x += speed;
+       }
+       if (letter == "n"){
+         player += 1;
+         if (player >= squid.length){
+           player = 0;
+         }
+       }
      }
 
-     function moveUp(){
-       y -= 10;
-     }
+
+
 
 })
