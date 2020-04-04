7084a95ed4e4ba9d7aac2bca38b966852f7017e7
Julia Ballas <42751526+jballas@users.noreply.github.com>
2020-04-04 10:07:11 -0600

    moved CLASS to new file

--- HW-11/script.js
+++ HW-11/script.js
@@ -30,39 +30,6 @@ var squares =  {"info":[{"x" : 10,
 var squid = new Array();
 var player = 0;
 
-class Vertibrate {
-
-  constructor(x, y, width, height, color){
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
-    this.color = color;
-  }
-
-  get vert_x(){
-    return this.x;
-  }
-
-  set vert_x(x){
-    this.x = x;
-  }
-
-  get vert_y(){
-    return this.y;
-  }
-
-  set vert_y(y){
-    this.y = y;
-  }
-
-
-  toString(){
-    let str;
-    str = this.x + this.y + this.width + this.height + this.color
-    return str;
-  }
-}
 
     var canvas = document.getElementById("canvas");
     var ctx = canvas.getContext("2d");
---
+++ HW-11/vertibrate.js
@@ -0,0 +1,28 @@
+class Vertibrate {
+
+  constructor(x, y, width, height, color){
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+  }
+
+  get vert_x(){
+    return this.x;
+  }
+
+  set vert_x(x){
+    this.x = x;
+  }
+
+  get vert_y(){
+    return this.y;
+  }
+
+  set vert_y(y){
+    this.y = y;
+  }
+
+
+}
