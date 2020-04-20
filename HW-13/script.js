35a8aa8930d27a8ad7ef6618983757d25d1f6f6c
Julia Ballas <42751526+jballas@users.noreply.github.com>
2020-04-20 17:11:14 -0600

    Update script.js
    final edits

--- HW-13/script.js
+++ HW-13/script.js
@@ -66,14 +66,20 @@ function loadFont() {
                           bevelOffset: 0,
                           bevelSegments: 0
                     });
+                    //
+                  //  textGeo.computeBoundingBox(); // This keeps the text in the middle of the scene
+                  //  textGeo.computeVertexNormals();
+
 
                     // This will adjust the color of the text
                     var color = new THREE.Color(0x062502);
                     var textMaterial = new THREE.MeshBasicMaterial({
-                        color: color // Text color created as a material
+                        color: color // Text color as a material
                     });
 
                     text = new THREE.Mesh(textGeo, textMaterial)
+                  //  text.position.x = -textGeo.boundingBox.max.x / 2;
+                    text.castShadow = true;
                     text.position.set(0,20,10); // moves the position of the text
                     scene.add(text)
                 }
